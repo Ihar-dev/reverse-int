c20acb887ff62e013703367bd853fb0c3e4a06ca
Ihar Kozik <ihar.kozik@gmail.com>
2021-08-14 17:57:48 +0300

    feat: edit index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let arr = [];
+    (n < 0 ? arr = (-n).toString().split('') : arr = n.toString().split(''));
+    let outputArr = [];
+    for (let i = 0; i < n.toString().length; i += 1) {
+        outputArr.push(arr[n.toString().length - 1 - i]);
+    }
+    return Number(outputArr.join(''));
 }
